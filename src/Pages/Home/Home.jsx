c1d247970d6b8d1580623ec933a2795070b5a127
moktadir-mirul <moktadir.mirul@gmail.com>
2025-07-17 22:01:36 +0600

    some basics set up done

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from '../../App';
+
+const Home = () => {
+    return (
+        <div>
+            I am home.
+            <App></App>
+        </div>
+    );
+};
+
+export default Home;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
+import Home from "../Pages/Home/Home";
 
 export const Router = createBrowserRouter([
     {
         path: "/",
-        Component: App
+        Component: Home
     }
 ])
